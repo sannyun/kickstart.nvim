0d72af691c4e19009a71a9fce32d9d3df02f842e
a.iuniaev <sannyun@voutlook.com>
2025-07-25 10:26:28 +0300

    Make use of relative line numbers

--- lua/options.lua
+++ lua/options.lua
@@ -15,7 +15,7 @@ vim.o.confirm = true
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
