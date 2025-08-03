b2a325e98a9361f19ab5470d883090450492212d
Aleksandr Iuniaev <sannyun@outlook.com>
2025-08-03 15:50:14 +0300

    Simplify switching between toggleterm directions

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,8 +3,16 @@ return {
     'akinsho/toggleterm.nvim',
     config = function()
       require('toggleterm').setup {
+        size = function(term)
+          if term.direction == 'horizontal' then
+            return 20
+          elseif term.direction == 'vertical' then
+            return vim.o.columns - vim.o.colorcolumn
+          end
+        end,
+
         open_mapping = [[<C-\>]],
-        direction = 'float',
+        direction = 'horizontal',
         float_opt = {
           border = 'curved',
         },
@@ -15,11 +23,14 @@ return {
           end,
         },
         close_on_exit = false,
+        persist_mode = true,
       }
+
+      vim.keymap.set('n', '<leader>ttn', ':TermNew<cr>', { desc = '[T]oggle[T]erm [N]ew' })
+      vim.keymap.set('n', '<leader>tts', ':TermSelect<cr>', { desc = '[T]oggle[T]erm [S]elect' })
+      vim.keymap.set('n', '<leader>tth', ':ToggleTerm<cr>:ToggleTerm direction=horizontal<cr>', { desc = '[T]oggle[T]erm [H]orizontal' })
+      vim.keymap.set('n', '<leader>ttv', ':ToggleTerm<cr>:ToggleTerm direction=vertical<cr>', { desc = '[T]oggle[T]erm [V]ertical' })
+      vim.keymap.set('n', '<leader>ttf', ':ToggleTerm<cr>:ToggleTerm direction=float<cr>', { desc = '[T]oggle[T]erm [F]loat' })
     end,
-    keys = {
-      { '<leader>tn', '<cmd>TermNew<cr>', desc = '[T]oggleTerm [N]ew' },
-      { '<leader>ts', '<cmd>TermSelect<cr>', desc = '[T]oggleTerm [S]elect' },
-    },
   },
 }
