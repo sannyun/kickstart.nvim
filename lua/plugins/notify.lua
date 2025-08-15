16810d5efabb5e4449810ad41e189c1bfa41d7e5
Aleksandr Iuniaev <sannyun@outlook.com>
2025-08-15 20:17:52 +0300

    Add key binding to dismiss active notifications

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,6 +3,11 @@ return {
     'rcarriga/nvim-notify',
     config = function()
       vim.notify = require 'notify'
+      local notify = require 'notify'
+
+      vim.keymap.set('n', '<leader>nd', function()
+        notify.dismiss { pending = true, silent = false }
+      end, { desc = '[N]otify [D]ismiss' })
     end,
   },
 }
