705373215c2e93cfcc033b7c17b7951a7d0d038d
Aleksandr Iuniaev <sannyun@outlook.com>
2025-11-18 23:43:01 +0200

    Set up TODO icons

--- init.lua
+++ init.lua
@@ -236,7 +236,14 @@ require('lazy').setup({
   },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = true, keywords = {
+      NOTE = { icon = '' },
+    } },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
