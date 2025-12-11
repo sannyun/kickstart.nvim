ad2a8e26fc58bc5815746af072bb1983991dacfe
Aleksandr Iuniaev <sannyun@outlook.com>
2025-12-11 23:47:21 +0200

    Update DAP configuration

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,39 +7,117 @@ return {
     },
   },
   {
-    'rcarriga/nvim-dap-ui',
-    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
+    },
+    keys = {
+      {
+        '<F5>',
+        function()
+          require('dap').continue()
+        end,
+        desc = 'Debug: Start/Continue',
+      },
+      {
+        '<F1>',
+        function()
+          require('dap').step_into()
+        end,
+        desc = 'Debug: Step Into',
+      },
+      {
+        '<F2>',
+        function()
+          require('dap').step_over()
+        end,
+        desc = 'Debug: Step Over',
+      },
+      {
+        '<F3>',
+        function()
+          require('dap').step_out()
+        end,
+        desc = 'Debug: Step Out',
+      },
+      {
+        '<leader>b',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'Debug: Toggle Breakpoint',
+      },
+      {
+        '<leader>B',
+        function()
+          require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end,
+        desc = 'Debug: Set Breakpoint',
+      },
+      {
+        '<leader>dk',
+        function()
+          require('dap').terminate()
+        end,
+        desc = 'DAP [K]ill',
+      },
+      {
+        '<leader>dd',
+        function()
+          require('dapui').toggle()
+        end,
+        desc = 'DAP [T]oggle',
+      },
+    },
     config = function()
       local dap = require 'dap'
       local dapui = require 'dapui'
-      dapui.setup()
 
-      dap.listeners.after.event_initialized['dapui_config'] = function()
-        dapui.open {}
+      require('mason-nvim-dap').setup {
+        automatic_installation = true,
+        handlers = {},
+        ensure_installed = {
+          'codelldb',
+        },
+      }
+
+      -- Dap UI setup
+      -- For more information, see |:help nvim-dap-ui|
+      dapui.setup {
+        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        controls = {
+          icons = {
+            pause = '⏸',
+            play = '▶',
+            step_into = '⏎',
+            step_over = '⏭',
+            step_out = '⏮',
+            step_back = 'b',
+            run_last = '▶▶',
+            terminate = '⏹',
+            disconnect = '⏏',
+          },
+        },
+      }
+
+      -- Change breakpoint icons
+      vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+      vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+      local breakpoint_icons = vim.g.have_nerd_font
+          and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+        or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+      for type, icon in pairs(breakpoint_icons) do
+        local tp = 'Dap' .. type
+        local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+        vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
       end
-      dap.listeners.before.event_terminated['dapui_config'] = function()
-        dapui.close {}
-      end
 
-      vim.keymap.set('n', '<leader>dd', function()
-        dapui.toggle()
-      end)
-    end,
-  },
-  {
-    'mfussenegger/nvim-dap',
-    dependencies = 'williamboman/mason.nvim',
-    config = function()
-      local dap = require 'dap'
-
-      -- dap.set_log_level 'TRACE'
-      -- dap.adapters.codelldb = {
-      --   type = 'executable',
-      --   command = vim.fn.exepath 'codelldb',
-      --
-      --   -- On windows you may have to uncomment this:
-      --   detached = false,
-      -- }
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
       dap.adapters.codelldb = {
         type = 'server',
@@ -79,11 +157,6 @@ return {
         },
       }
     end,
-    keys = {
-      { '<leader>dc', '<cmd>DapContinue<cr>', desc = 'DAP [C]ontinue' },
-      { '<leader>db', '<cmd>DapToggleBreakpoint<cr>', desc = 'DAP Toggle [b]reakpoint' },
-      { '<leader>dt', '<cmd>DapTerminate<cr>', desc = 'DAP [T]erminate' },
-    },
   },
   {
     'lucaSartore/nvim-dap-exception-breakpoints',
