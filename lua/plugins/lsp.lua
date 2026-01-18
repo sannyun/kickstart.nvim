516ff091f3bab437c1ef4e8a6ac59cb5c4f546eb
Aleksandr Iuniaev <sannyun@outlook.com>
2026-01-18 18:03:12 +0200

    Work around arbitrary cursor jumps when entering insert mode and pressing Tab

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -391,6 +391,34 @@ return {
       },
       'folke/lazydev.nvim',
     },
+    config = function(_, opts)
+      -- stop snippets when you leave to normal mode
+      local group = vim.api.nvim_create_augroup('LuaSnipLeaveSnippet', { clear = true })
+      -- Unlink current snippet on leaving insert/select mode
+      -- https://github.com/L3MON4D3/LuaSnip/issues/258#issuecomment-1011938524
+      vim.api.nvim_create_autocmd('ModeChanged', {
+        desc = 'Unlink current snippet on leaving insert/selection mode.',
+        group = vim.api.nvim_create_augroup('LuaSnipModeChanged', {}),
+        pattern = '[si]*:[^si]*',
+        -- Blink.cmp will enter normal mode shortly on accepting snippet completion,
+        -- see https://github.com/Saghen/blink.cmp/issues/2035
+        -- We don't want to unlink the current snippet in that case, as a workaround
+        -- wait a short time after leaving insert/select mode and unlink current
+        -- snippet if still not inside insert/select mode
+        callback = vim.schedule_wrap(function(args)
+          local ls = require 'luasnip'
+
+          if vim.fn.mode():match '^[si]' then -- still in insert/select mode
+            return
+          end
+          if ls.session.current_nodes[args.buf] and not ls.session.jump_active then
+            ls.unlink_current()
+          end
+        end),
+      })
+
+      require('blink.cmp').setup(opts)
+    end,
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
     opts = {
