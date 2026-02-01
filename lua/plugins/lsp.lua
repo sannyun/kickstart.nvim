86cf59ddef923b31d69bdfeff12e0391735fe889
Aleksandr Iuniaev <sannyun@outlook.com>
2026-02-01 16:58:00 +0200

    Add wezterm-types library to lazydev.nvim configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,7 +8,13 @@ return {
       library = {
         -- Load luvit types when the `vim.uv` word is found
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        { path = 'wezterm-types', mods = { 'wezterm' } },
       },
+    },
+    dependencies = {
+      'DrKJeff16/wezterm-types',
+      lazy = true,
+      version = false, -- Get the latest version
     },
   },
   {
